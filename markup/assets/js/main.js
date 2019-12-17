08d73181771c1bfbd69bc1f058ca5a03e433eb2f
Developer <walteronew@gmail.com>
2019-12-17 18:52:08 +0200

    first commit

---
+++ markup/assets/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*
+    This file can be used as entry point for webpack!
+ */
+
+// Import jQuey
+import $ from 'jquery';
+
+// Import Uikit
+import UIkit from "uikit";
+import Icons from 'uikit/dist/js/uikit-icons';
+// loads the Icon plugin
+UIkit.use(Icons);
+window.UIkit = UIkit;
---
+++ tars-config.js
@@ -0,0 +1,66 @@
+module.exports = {
+    "postcss": [],
+    "svg": {
+        "active": true,
+        "workflow": "sprite",
+        "symbolsConfig": {
+            "loadingType": "inject",
+            "usePolyfillForExternalSymbols": true,
+            "pathToExternalSymbolsFile": ""
+        }
+    },
+    "css": {
+        "workflow": "concat"
+    },
+    "js": {
+        "workflow": "modular",
+        "bundler": "webpack",
+        "lint": true,
+        "useBabel": true,
+        "removeConsoleLog": true,
+        "webpack": {
+            "useHMR": false,
+            "providePlugin": {}
+        },
+        "jsPathsToConcatBeforeModulesJs": [
+
+        ],
+        "lintJsCodeBeforeModules": false,
+        "jsPathsToConcatAfterModulesJs": [],
+        "lintJsCodeAfterModules": false
+    },
+    "sourcemaps": {
+        "js": {
+            "active": true,
+            "inline": true
+        },
+        "css": {
+            "active": true,
+            "inline": true
+        }
+    },
+    "notifyConfig": {
+        "useNotify": false,
+        "title": "TARS notification",
+        "sounds": {},
+        "taskFinishedText": "Task finished at: "
+    },
+    "minifyHtml": false,
+    "generateStaticPath": true,
+    "devPath": "./dev/",
+    "buildPath": "./builds/",
+    "useBuildVersioning": true,
+    "useArchiver": true,
+    "ulimit": 4096,
+    "templater": "handlebars",
+    "cssPreprocessor": "less",
+    "useImagesForDisplayWithDpi": [
+        96
+    ],
+    "fs": {
+        "staticFolderName": "assets",
+        "imagesFolderName": "images",
+        "componentsFolderName": "components"
+    },
+    "staticPrefix": "showcoins/"
+};
