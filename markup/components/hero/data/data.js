0778a8258ed74f52bd644d761c9c456cabda4a98
Mr.Simon <walteronew@gmail.com>
2019-12-20 00:37:14 +0200

    dev hero

---
+++ markup/components/hero/data/data.js
@@ -0,0 +1,78 @@
+var data = {
+    hero_block: {
+        items: [
+            {
+                title: "orker",
+                firstletter: "W",
+                subtitle: 'Mining',
+                text: 'No registration need, only your wallet addres',
+                button_text: 'Enter by you wallet',
+                info: [
+                    {
+                        title: 'Miners Online',
+                        icon: 'icon-miners.svg',
+                        count: 2
+                    },
+                    {
+                        title: 'Total Coins',
+                        icon: 'icon-coins.svg',
+                        count: 4
+                    }
+                ]
+            },
+            {
+                title: "taking",
+                firstletter: "S",
+                subtitle: 'Staking',
+                text: 'Activation WSM Key. Setup stratum with WSM key to hold on PoS',
+                button_text: 'Register or sign in',
+                info: [
+                    {
+                        title: 'Users Online',
+                        icon: 'icon-miners.svg',
+                        count: 2
+                    },
+                    {
+                        title: 'Total Coins',
+                        icon: 'icon-coins.svg',
+                        count: 4
+                    }
+                ]
+            },
+            {
+                title: "asternode",
+                firstletter: "M",
+                subtitle: 'Masternode',
+                text: 'PoW and PoS coins go to a shared masternode or full node',
+                button_text: 'Create masternode',
+                info: [
+                    {
+                        title: 'Users Online',
+                        icon: 'icon-miners.svg',
+                        count: 2
+                    },
+                    {
+                        title: 'MN Online',
+                        icon: 'icon-mn.svg',
+                        count: 4
+                    }
+                ]
+            },
+            {
+                title: "xchange",
+                firstletter: "E",
+                subtitle: 'Some text',
+                text: 'At any stage of mining, we will exchange without delay a coin in BTC if it is traded on friendly exchange',
+                button_text: 'Go to our exchange',
+                center: true,
+                info: [
+                    {
+                        title: 'BTC volume',
+                        icon: 'icon-bitcoin.svg',
+                        count: '= 263,19 BTC'
+                    }
+                ]
+            },
+        ]
+    }
+}
