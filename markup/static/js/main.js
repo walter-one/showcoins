680cc9eea50cefcfc272b7d04e2cdf16544d0b57
Mr.Simon <walteronew@gmail.com>
2020-01-19 21:33:01 +0200

    dev algo page v2

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -7,6 +7,7 @@
 // Import jQuey
 import $ from 'jquery';
 
+
 // Import Uikit
 import UIkit from "uikit";
 import Icons from 'uikit/dist/js/uikit-icons';
@@ -52,5 +53,33 @@ $('.uk-card-wallet table .uk-cell-close').on('click', function(){
 
 $('.uk-toggle').click(function(){
   $(this).toggleClass('uk-active');
-})
+});
+
+$('.uk-card-algos').on('click', function(){
+    var offset = $( this ).offset();
+    var height = $( this ).outerHeight();
+    var calcTop = offset.top + height + 20;
+    var heightContentTable = $(this).next().outerHeight();
+    var heightContent = $('.uk-content-algos .uk-content').outerHeight();
+
+    $('.uk-content-algos .uk-content').height(heightContent + heightContentTable);
+    $( this ).next().css('top', calcTop );
+    $(this).next().toggleClass('uk-active');
+    $('.uk-overlay').toggleClass('uk-active');
+    $(this).parent().toggleClass('uk-active');
+
+
+
+    console.log(heightContent);
+});
+$('.uk-overlay').on('click', function(){
+    $(this).removeClass('uk-active');
+    $('.uk-grid-small > div').removeClass('uk-active');
+    $('.uk-table-algos').removeClass('uk-active');
+});
+
+$('.uk-table-title').on('click', function(){
+    $(this).toggleClass('uk-active');
+    $(this).next().toggle( "slow" );
+});
 
