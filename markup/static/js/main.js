814bfa95773b3304d9ead0685fc44fb10a5ca02c
Mr.Simon <walteronew@gmail.com>
2020-01-22 01:30:02 +0200

    dev algo page v3

--- markup/static/js/main.js
+++ markup/static/js/main.js
@@ -55,31 +55,114 @@ $('.uk-toggle').click(function(){
   $(this).toggleClass('uk-active');
 });
 
-$('.uk-card-algos').on('click', function(){
-    var offset = $( this ).offset();
-    var height = $( this ).outerHeight();
-    var calcTop = offset.top + height + 20;
-    var heightContentTable = $(this).next().outerHeight();
-    var heightContent = $('.uk-content-algos .uk-content').outerHeight();
 
-    $('.uk-content-algos .uk-content').height(heightContent + heightContentTable);
-    $( this ).next().css('top', calcTop );
-    $(this).next().toggleClass('uk-active');
-    $('.uk-overlay').toggleClass('uk-active');
-    $(this).parent().toggleClass('uk-active');
+var heightTable = {
+    state: {
+        'content': '.uk-content-algos .uk-holder-grid',
+        'overlay': '.uk-overlay',
+        'height_table': false,
+        'item': false,
+        'top_height': false
+    },
+    init: function () {
 
+        var self = this;
 
+        this.calcHeightTable();
+        this.updateHeight();
+        this.updateOverlay();
+        this.updateAccordion(this.state.item);
 
-    console.log(heightContent);
-});
-$('.uk-overlay').on('click', function(){
-    $(this).removeClass('uk-active');
-    $('.uk-grid-small > div').removeClass('uk-active');
-    $('.uk-table-algos').removeClass('uk-active');
-});
+    },
+    calcHeightTable: function ($item) {
+        var $this = $($item).parent();
+        this.state.height_table = $this.find('.uk-table-algos > table').height();
+    },
+    calcHeightContent: function () {
+        var contentHeight = $(this.state.content);
+        var heigtGrid = contentHeight.offset().top,
+        tableH = this.state.top_height-heigtGrid+this.state.height_table;
 
-$('.uk-table-title').on('click', function(){
-    $(this).toggleClass('uk-active');
-    $(this).next().toggle( "slow" );
-});
+        contentHeight.css('min-height', tableH);
+    },
+    calcPosition: function ($item) {
+        var $this = $($item),
+        offset = $this.offset(),
+        height = $this.outerHeight();
+        this.state.top_height = offset.top + height + 20;
+        $this.next().css('top', this.state.top_height );
+    },
+    pageAddClass: function ($item) {
+
+        var $this = $($item);
+
+        if($this.hasClass('uk-active')) {
+            $this.removeClass('uk-active');
+        }else {
+            $this.addClass('uk-active');
+        }
+
+    },
+    showContent: function($item) {
+
+        var $this = $($item).parent();
+
+        if($this.hasClass('uk-active')) {
+            $this.removeClass('uk-active');
+        }else {
+            $this.addClass('uk-active');
+        }
+    },
+    removeHeight: function () {
+        var $this = $(this.state.content);
+
+        if(!$(this.state.overlay).hasClass('uk-active')){
+            $this.css('min-height', 'inherit');
+        }
+    },
+    updateHeight: function () {
+        var self = this;
+
+        $('.uk-card-algos').on('click', function(){
+
+            self.state.item = $(this);
+
+            self.pageAddClass(self.state.overlay);
+            self.showContent(self.state.item);
+            self.calcHeightTable(self.state.item);
+            self.calcPosition(self.state.item);
+            self.calcHeightContent();
+            self.removeHeight();
+
+        })
+    },
+    updateOverlay: function () {
+        var self = this;
+
+        $(this.state.overlay).on('click', function(){
+            self.pageAddClass(self.state.overlay);
+            self.showContent(self.state.item);
+            self.removeHeight();
+        })
+    },
+    updateAccordion: function ($item) {
+        var self = this;
+        $('.uk-table-title').on('click', function(){
+            var $this = $(this);
+            self.pageAddClass($this);
+
+            var accordContent = $this.next();
+            if($this.hasClass('uk-active')) {
+                accordContent.show();
+            }else {
+                accordContent.hide();
+            }
+
+            self.calcHeightTable(self.state.item);
+            self.calcHeightContent();
+            self.removeHeight();
+        })
+    }
+}
+heightTable.init();
 
