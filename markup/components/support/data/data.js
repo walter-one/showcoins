5af8840b63088cc97eaf2ecac48650f224a88b53
Mr.Simon <walteronew@gmail.com>
2020-02-09 12:40:08 +0200

    wms overview

---
+++ markup/components/support/data/data.js
@@ -0,0 +1,213 @@
+var data = {
+    support: {
+        items: [
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+            {
+                title: "1X2 Coin",
+                per: '71%',
+                pow: 29,
+                pos: 84,
+                mn: 190,
+                labels:[
+                    {
+                        icon: "ico-change.svg",
+                    },
+                    {
+                        icon: 'ico-wsm.svg',
+                    },
+                    {
+                        icon: 'icon-mn2.svg',
+                    }
+                ],
+                default: '57%',
+                holding: 'pay rewards',
+                balance: '04 day',
+                required: '1000 1x2',
+                pool: 0.486457116
+            },
+        ]
+    }
+}
