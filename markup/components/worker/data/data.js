9a75b081ffa8fba5ddccb1db3b9bfb1c44e20857
Mr.Simon <walteronew@gmail.com>
2020-03-25 21:46:54 +0200

    add worker page

---
+++ markup/components/worker/data/data.js
@@ -0,0 +1,26 @@
+var data = {
+    worker_card: {
+        items: [
+            {
+                ico: "icon-curve.svg",
+                title: "Hashrate",
+                text: "3212312312312312 mH/S",
+            },
+            {
+                ico: "icon-perfomance.svg",
+                title: "Performance",
+                text: "20% rejection",
+            },
+            {
+                ico: "icon-miners.svg",
+                title: "Miners",
+                text: "32 mH/S",
+            },
+            {
+                ico: "icon-share.svg",
+                title: "Shares",
+                text: "35,42%",
+            },
+        ]
+    }
+}
